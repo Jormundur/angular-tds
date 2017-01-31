9efdf721dddc0822a21face77e7731be6a7985ef
Jormundur <clement.cormier@hotmail.fr>
2017-01-31 15:00:37 +0100

    Commit fin note

--- TD1_ang/app/app.js
+++ TD1_ang/app/app.js
@@ -1,15 +1,34 @@
 
 
-var myAppModule = angular.module('myAppModule', []);
+var myAppModule = angular.module('myAppModule', ['ngCookies']);
 
 
-myAppModule.controller("texteController",function() {
-      this.nom = "";
+myAppModule.controller("texteController",["$cookies",function($cookies){
+      this.nom = $cookies.get('test') ||"";
+      this.vnote = "";
       var self = this;
       this.count = function () {
           return 100-self.nom.length;
       };
-      this.save = function(nom){
-        this.nom = nom;
+      this.clear = function(){
+        this.nom = "";
+        this.vnote = "";
       };
-});
+      this.save = function(){
+          this.vnote = "Note sauvegardée";
+          $cookies.put('test',this.nom);
+      };
+    this.change = function(){
+        this.vnote = "Note changée";
+    };
+    this.couleur = function(){
+        if(this.vnote == "Note sauvegardée")
+            return "alert-success";
+        if(self.nom.length >= 50 && self.nom.length < 80)
+            return "alert-warning";
+        if(self.nom.length >= 80)
+            return "alert-danger";
+        else
+            return "alert-success";
+                };
+}]);

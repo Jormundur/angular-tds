cb7e65e58ae99860fc4cdac1bf714b9f8aba0255
Jormundur <clement.cormier@hotmail.fr>
2017-01-31 16:00:28 +0100

    commit 2eme proj

---
+++ TD1_2/app/app.js
@@ -0,0 +1,27 @@
+
+
+var servicesModule = angular.module('servicesApp', []);
+
+
+servicesApp.controller("servicesController",function(){
+  this.services =
+  [
+        {
+            "name": "Web Development",
+            "price": 300,
+            "active":true
+        },{
+        "name": "Design",
+        "price": 400,
+        "active":false
+    },{
+        "name": "Integration",
+        "price": 250,
+        "active":false
+    },{
+        "name": "Formation",
+        "price": 220,
+        "active":false
+    }
+    ]
+});

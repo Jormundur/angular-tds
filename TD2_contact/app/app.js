027a8d30ba3154132223e4a604ff77b08dc6b4d7
Jormundur <clement.cormier@hotmail.fr>
2017-02-21 16:34:54 +0100

    Commit fin Contact
    Fin de l'application contact avec l'ajout de la modification et de
    l'ajout. Correction du filtre et ajout des 2 compteurs.

--- TD2_contact/app/app.js
+++ TD2_contact/app/app.js
@@ -3,7 +3,7 @@
 var ContactApp = angular.module('ContactApp', []);
 
 
-ContactApp.controller("ContactController",function(){
+ContactApp.controller("ContactController", function (){
     var self = this;
     this.operation = "Ajouter";
     this.visible = false;
@@ -13,40 +13,97 @@ ContactApp.controller("ContactController",function(){
             "nom": "ZUCKERBERG",
             "prenom": "Mark",
             "mail": "mark@facebook.com",
-            "delete" : "false"
+            "delete" : false
         },
         {
             "nom": "GATES",
             "prenom": "Bill",
             "mail": "bill@microsoft.com",
-            "delete" : "false"
+            "delete" : false
 
         },
         {
             "nom": "JOBS",
             "prenom": "Steeve",
             "mail": "Steeve@apple.com",
-            "delete" : "false"
+            "delete" : false
 
         }]
+    this.contact = [];
 
-    this.changeOpe=function(){
+this.tmpContact =
+    {
+        "nom" : "",
+        "prenom" : "",
+        "mail" : "",
+    }
+
+    this.modifContact=function(contact){
+    self.contact = contact;
         self.visible = true;
-        if(self.operation == "Ajouter")
-          self.operation = "Modifier";
-        else
-          self.operation = "Ajouter";
+        self.operation = "Modifier";
+        self.tmpContact.nom = contact.nom;
+        self.tmpContact.prenom = contact.prenom;
+        self.tmpContact.mail = contact.mail;
+
+    }
+
+    this.changeAjout=function(){
+        self.visible = true;
+        self.operation = "Ajouter";
     };
+
+this.addContact=function(){
+    if(self.operation == "Modifier")
+        self.modif();
+    else {
+        self.contacts.push(angular.copy(self.tmpContact));}
+        self.visible = false;
+        self.tmpContact.nom = "";
+        self.tmpContact.prenom = "";
+        self.tmpContact.mail = "";
+
+    };
+
+this.modif=function(){
+    self.contact.nom = angular.copy(self.tmpContact.nom);
+    self.contact.prenom = angular.copy(self.tmpContact.prenom);
+    self.contact.mail = angular.copy(self.tmpContact.mail);
+
+};
+
     this.setInvisible=function(){
         self.visible = false;
     };
     this.setDelete=function(contact){
             self.isDelete = 1;
-            contact.delete = "true";
+            contact.delete = true;
     };
 
+    this.countDel=function(){
+        var compteur = 0;
+        for (var i = 0;i<self.contacts.length;i++){
+            if(self.contacts[i].delete == true)
+                compteur++;
+        }
+        return compteur;
+
+
+            }
     this.cancelDelete=function(){
         self.isDelete = 0;
-        contacts.delete = "true";
+        for (var i = 0;i<self.contacts.length;i++)
+        self.contacts[i].delete = false;
+    };
+});
+
+ContactApp.filter('filtring',function(){
+    return function(items){
+        var result=[];
+        for(var i=0;i<items.length;i++){
+            if(!items[i].delete)
+                result.push(items[i]);
+        }
+        return result;
     };
 });

520cc864cb5f35dcd6f92deda619c2884f2678ba
Jormundur <clement.cormier@hotmail.fr>
2017-02-28 15:22:30 +0100

    Commit Fin conversion
    Fin de la partie conversion en rajoutant le nécessaire dans app.js et
    des ng-models dans l'index. Reste la partie historique

--- CurrencyApp/app/app.js
+++ CurrencyApp/app/app.js
@@ -1,33 +1,9 @@
 'use strict';
 
-var CurrencyApp = angular.module('CurrencyApp', []);
-
-angular.module("CurrencyApp").controller("currencyController",['$http', function($http) {
-
-    this.currencies= "";
-    this.from= "";
-    this.to= "";
-    this.what= "";
-    this.result= "";
+//var CurrencyApp = angular.module('CurrencyApp', []);
 
 
-    $http.get('app/data/currencymap.json').
-        then(function(data, status, headers, config) {
-        self.currencies = data;
-    }).
-    error(function(data, status, headers, config) {
-        console.log("Erreur avec le statut Http : "+status);
-    });
-
-
-
-
-
-
-
-}]);
-
-angular.module('httpExample', [])
+angular.module('CurrencyApp', [])
     .config(['$sceDelegateProvider', function($sceDelegateProvider) {
         // We must whitelist the JSONP endpoint that we are using to show that we trust it
         $sceDelegateProvider.resourceUrlWhitelist([
@@ -36,8 +12,45 @@ angular.module('httpExample', [])
         ]);
     }]);
 
-$http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+from.code+'_'+to.code, {jsonpCallbackParam: 'callback'})
-    .then(function(response) {
-        self.result=response.data[self.from.code+'_'+self.to.code].val;
-       // ...
-    });
+
+
+
+
+
+
+angular.module("CurrencyApp").controller("currencyController",['$http', function($http) {
+    var self = this;
+    this.from;
+    this.to;
+    this.what= 1;
+    this.result;
+    this.temp;
+
+    $http.get('data/currencymap.json').
+    then(function(response) {
+            self.currencies = response.data;
+            self.from = self.currencies.EUR;
+            self.to = self.currencies.USD;
+
+        },
+        function(response) {
+            console.log("Erreur avec le statut Http : "+response.status);
+        });
+    this.swap=function(){
+        self.temp = self.from;
+        self.from = self.to;
+        self.to = self.temp;
+    };
+
+    this.getResult=function(){
+        $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from.code+'_'+self.to.code, {jsonpCallbackParam: 'callback'})
+            .then(function(response) {
+                self.result=response.data[self.from.code+'_'+self.to.code].val*self.what;
+               // ...
+            });
+  };
+
+}]);
+
+
+

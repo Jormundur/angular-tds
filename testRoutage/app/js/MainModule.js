715e25fb942df022d40856fe04eb3079034aaefb
Jormundur <clement.cormier@hotmail.fr>
2017-03-14 15:52:21 +0100

    Commit fin td5

--- testRoutage/app/js/MainModule.js
+++ testRoutage/app/js/MainModule.js
@@ -1,3 +1,6 @@
+
+angular.module("sampleApp", ['ngRoute']);
+
 angular.module("sampleApp").config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
--- testRoutage/app/js/controllers/MainController.js
+++ testRoutage/app/js/controllers/MainController.js
@@ -1,3 +1,5 @@
+
+
 angular.module("sampleApp").controller("MainController",[function(){
     this.content1="Contenu du template de route1"
 }]);
